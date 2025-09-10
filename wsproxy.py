d26f9d6da80216cea6031c6d17112060183f926f
stellawills <76977857+stellawills@users.noreply.github.com>
2025-09-10 21:16:46 +0100

    Update wsproxy.py

--- wsproxy.py
+++ wsproxy.py
@@ -7,7 +7,7 @@ LISTENING_ADDR = '0.0.0.0'
 try:
    LISTENING_PORT = int(sys.argv[1])
 except:
-   LISTENING_PORT = 80
+   LISTENING_PORT = 25
 SSH_PORT = 22  # Default SSH port
 BUFLEN = 4096 * 4
 TIMEOUT = 60
